27a5a408d6690f21baf026bbd3862695303d9afc
Yeison Yela <yeisonyelag@gmail.com>
2022-05-25 09:17:13 -0500

    Se realizo cambio en el html de la carpeta inicio para el diseño

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fundido } from '../animation';
 
 @Component({
   selector: 'inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
+  animations: [fundido]
 })
-export class AppComponent {
-  title = 'Tienda-Montechelo-Angular';
+export class InicioComponent implements OnInit {
+  title = 'Bienvenido Al Inicio';
+
+  ngOnInit(){
+    console.log("inicio.component cargado !!");
+    }
 }
+
+
+
+
