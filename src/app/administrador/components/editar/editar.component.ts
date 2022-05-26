9d2ef1d4f397acc6d4eac0bc61635fea8d05c738
Cristian Jaimes <cristianjaimes691@gmail.com>
2022-05-26 09:41:28 -0500

    Rutas Administradoras

---
+++ src/app/administrador/components/editar/editar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editar',
+  templateUrl: './editar.component.html',
+  styleUrls: ['./editar.component.css']
+})
+export class EditarComponent {
+  title = 'editar';
+}
---
+++ src/app/servicios/admin.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+//import { UserService } from "./user.service";
+
+@Injectable()
+
+export class AdminGuard{
+    constructor(){}
+}
